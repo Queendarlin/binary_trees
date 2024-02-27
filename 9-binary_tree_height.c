9078b3a4e36ebc767439f8a2163e124021e1dd01
Queendarlin <queendarlinnnamani@gmail.com>
2024-02-27 06:47:29 -0800

    Task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_left, height_right;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	height_left = binary_tree_height(tree->left);
