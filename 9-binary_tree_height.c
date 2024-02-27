3ba2e14b6b11038d9578f02919d73789cb817999
Queendarlin <queendarlinnnamani@gmail.com>
2024-02-27 06:30:40 -0800

    Task9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left, height_right;
+
+	if (tree == NULL)
+		return (0);
+
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+
+	/* Return the maximum height between the left and right subtrees,
+	 * plus one to account for the current node */
+	return (height_left > height_right ? height_left + 1 :
+			height_right + 1);
+}
