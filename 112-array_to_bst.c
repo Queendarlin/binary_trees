e2175323da00704e5bcd1c9e28e5cf4c590ca2c8
Queendarlin <queendarlinnnamani@gmail.com>
2024-02-28 12:58:27 -0800

    Task 26

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -12,7 +12,7 @@ bst_t *array_to_bst(int *array, size_t size)
 	bst_t *root = NULL;
 	size_t i;
 
-	if (!array || size == 0)
+	if (array || size == 0)
 		return (NULL);
 
 	/* Insert each element of the array into the BST */
